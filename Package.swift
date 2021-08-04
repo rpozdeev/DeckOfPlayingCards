80582bcf900e13702929582ddedb689d684eada7
Roman I Pozdeev <rip@m2.ru>
2021-08-04 16:46:26 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["DeckOfPlayingCards"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rpozdeev/PlayingCard", from: "1.0.0"),
-        .package(url: "https://github.com/rpozdeev/FisherYates", from: "1.0.0"),
+        .package(url: "https://github.com/rpozdeev/PlayingCard.git", from: "1.0.0"),
+        .package(url: "https://github.com/rpozdeev/FisherYates.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
